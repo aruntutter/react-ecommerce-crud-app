c7096cc438b8403f64fac5455e676bb3bf393410
Arun Kumar R <aruntutter19@gmail.com>
2024-03-15 23:35:32 +0530

    HomePageProductCard added

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,12 +1,14 @@
 import Layout from "../../components/layout/Layout";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Category from "../../components/category/Category";
+import HomePageProductCard from "../../components/homePageProductCard/HomePageProductCard";
 
 const HomePage = () => {
   return (
     <Layout>
       <HeroSection />
       <Category />
+      <HomePageProductCard />
     </Layout>
   );
 };
