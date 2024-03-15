b5c1dab8ba8916779c4cb67ba132e150fe8f84f1
Arun Kumar R <aruntutter19@gmail.com>
2024-03-15 21:38:21 +0530

    category component displayed

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,12 @@
 import Layout from "../../components/layout/Layout";
 import HeroSection from "../../components/heroSection/HeroSection";
+import Category from "../../components/category/Category";
 
 const HomePage = () => {
   return (
     <Layout>
       <HeroSection />
+      <Category />
     </Layout>
   );
 };
