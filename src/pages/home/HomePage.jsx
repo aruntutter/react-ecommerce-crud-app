0aad76a5374c2924b1e442eab9a10313705768cb
Arun Kumar R <aruntutter19@gmail.com>
2024-03-16 00:24:50 +0530

    Track component added

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,6 +2,7 @@ import Layout from "../../components/layout/Layout";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Category from "../../components/category/Category";
 import HomePageProductCard from "../../components/homePageProductCard/HomePageProductCard";
+import Track from "../../components/track/Track";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <HeroSection />
       <Category />
       <HomePageProductCard />
+      <Track />
     </Layout>
   );
 };
