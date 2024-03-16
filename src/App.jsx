e1830e5d88dac39c817118277268f14bb14a98cc
Arun Kumar R <aruntutter19@gmail.com>
2024-03-16 20:57:57 +0530

    scroll to top functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/noPage/NoPage";
 import ProductInfo from "./pages/productInfo/ProductInfo";
+import ScrollTop from "./components/scrollTop/ScrollTop";
 
 const App = () => {
   return (
     <div>
       <Router>
+        <ScrollTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
