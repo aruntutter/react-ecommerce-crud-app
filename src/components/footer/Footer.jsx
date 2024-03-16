b75c6399df94a9f406d474150fc76a001f859114
Arun Kumar R <aruntutter19@gmail.com>
2024-03-16 13:46:29 +0530

    footer created and styled

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,34 @@
 import "./Footer.css";
+import Logo from "../../assets/images/ezbuy-logo.png";
+import { FaXTwitter, FaGithub, FaLinkedinIn } from "react-icons/fa6";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="footer">
+        <div className="left">
+          <div className="footer-logo">
+            <img src={Logo} alt="logo" />
+          </div>
+          <h2></h2>
+          <p>&copy; 2024 @ezBuy</p>
+        </div>
+        <div class="social-icons">
+          <ul>
+            <li>
+              <FaXTwitter />
+            </li>
+            <li>
+              <FaGithub />
+            </li>
+            <li>
+              <FaLinkedinIn />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
