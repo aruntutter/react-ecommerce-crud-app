14fc8cb0868b9435f592c1fd13148e0336741320
Arun Kumar R <aruntutter19@gmail.com>
2024-03-17 12:57:12 +0530

    route created for: Cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/noPage/NoPage";
 import ProductInfo from "./pages/productInfo/ProductInfo";
 import ScrollTop from "./components/scrollTop/ScrollTop";
+import CartPage from "./pages/cart/CartPage";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
           <Route path="/productinfo" element={<ProductInfo />} />
+          <Route path="/cart" element={<CartPage />} />
         </Routes>
       </Router>
     </div>
