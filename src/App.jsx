e95000297c172895d2047ee1a5ce7a8bc6f73672
Arun Kumar R <aruntutter19@gmail.com>
2024-03-18 12:34:37 +0530

    Admin Dashboard route created

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AllProduct from "./pages/allProduct/AllProduct";
 import Signup from "./pages/registration/signup/Signup";
 import Login from "./pages/registration/login/Login";
 import UserDashboard from "./pages/user/UserDashboard";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/user-dashboard" element={<UserDashboard />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
         </Routes>
       </Router>
     </div>
