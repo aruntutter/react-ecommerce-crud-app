6c0b397cbe1ea6349281e55d96cacdd6b67af552
Arun Kumar R <aruntutter19@gmail.com>
2024-03-19 00:20:43 +0530

    loading state created

--- src/context/myState.jsx
+++ src/context/myState.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import MyContext from "./myContext";
 
 const myState = ({ children }) => {
-  const name = "Arun Kumar";
+  // const name = "Arun Kumar";
+  const [loading, setLoading] = useState(false);
   return (
     <div>
-      <MyContext.Provider value={name}>{children}</MyContext.Provider>
+      <MyContext.Provider
+        value={{
+          loading,
+          setLoading,
+        }}
+      >
+        {children}
+      </MyContext.Provider>
     </div>
   );
 };
