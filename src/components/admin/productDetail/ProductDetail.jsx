a3e6806c9d8ebf428a44b45478796746dfd3c8f6
Arun Kumar R <aruntutter19@gmail.com>
2024-03-18 15:41:56 +0530

    productDetail Component created

---
+++ src/components/admin/productDetail/ProductDetail.jsx
@@ -0,0 +1,51 @@
+import "./ProductDetail.css";
+import { MdDeleteOutline } from "react-icons/md";
+import { MdOutlineEdit } from "react-icons/md";
+
+const ProductDetail = () => {
+  return (
+    <div className="product-detail">
+      <div className="product-detail-head">
+        <h4>All Products</h4>
+        <div className="add-product-btn">
+          <button>Add Product</button>
+        </div>
+      </div>
+      {/* Table */}
+      <div className="container">
+        <div className="table-wrapper">
+          {/* table  */}
+          <table>
+            <thead>
+              <tr>
+                <th className="custom-th">S.No</th>
+                <th className="custom-th">Location Name</th>
+                <th className="custom-th">Action</th>
+              </tr>
+            </thead>
+            {/* tbody */}
+            <tbody>
+              <tr>
+                <td>1.</td>
+                <td>Name</td>
+                <td>
+                  {/* Edit */}
+                  <button>
+                    <MdOutlineEdit />
+                  </button>
+                  {/* Delete */}
+                  <button>
+                    <MdDeleteOutline />
+                  </button>
+                </td>
+              </tr>
+              {/* More rows can be added here */}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetail;
