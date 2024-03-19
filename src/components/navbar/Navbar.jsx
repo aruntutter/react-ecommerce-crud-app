7c8279ae3e42d01935e17ce98fe04232852c3a6d
Arun Kumar R <aruntutter19@gmail.com>
2024-03-19 12:16:26 +0530

    nav list customize according to login and logout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 import "./Navbar.css";
 import SearchBar from "../searchBar/SearchBar";
 import Logo from "../../assets/images/ezbuy-logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { HiUser } from "react-icons/hi";
 
 const Navbar = () => {
+  const user = JSON.parse(localStorage.getItem("users"));
+  console.log(user);
+
+  // Navigate
+  const navigate = useNavigate();
+
+  // Logout
+  const logout = () => {
+    localStorage.clear("users");
+    navigate("/login");
+  };
   return (
     <div className="navbar">
       {/* Logo */}
@@ -25,17 +36,38 @@ const Navbar = () => {
             <Link to={"/allproduct"}>All</Link>
           </li>
           {/* Signup */}
-          <li>
-            <Link to={"/signup"}>Signup</Link>
-          </li>
+          {!user ? (
+            <li>
+              <Link to={"/signup"}>Signup</Link>
+            </li>
+          ) : (
+            ""
+          )}
           {/* Profile */}
-          <li>
-            <Link to={"/user-dashboard"}>
-              <div className="profile-pic">
-                <HiUser />
-              </div>
-            </Link>
-          </li>
+          {user?.role === "user" ? (
+            <li>
+              <Link to={"/user-dashboard"}>
+                <div className="profile-pic">
+                  <HiUser />
+                </div>
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link to={"/admin-dashboard"}>
+                <div className="profile-pic">
+                  <HiUser />
+                </div>
+              </Link>
+            </li>
+          )}
+
+          {/* Logout */}
+          {user && (
+            <li onClick={logout}>
+              <Link>Logout</Link>
+            </li>
+          )}
           {/* Cart */}
           <li>
             <Link to={"/cart"}>Cart-0</Link>
