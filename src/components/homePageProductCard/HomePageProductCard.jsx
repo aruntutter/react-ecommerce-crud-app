1caa0eda3d42ffaa8ff345a2b57afe0a33b329fe
Arun Kumar R <aruntutter19@gmail.com>
2024-03-19 19:47:50 +0530

    product displayed

--- src/components/homePageProductCard/HomePageProductCard.jsx
+++ src/components/homePageProductCard/HomePageProductCard.jsx
@@ -1,115 +1,42 @@
 import { useNavigate } from "react-router-dom";
 import "./HomePageProductCard.css";
-
-// productData
-const productData = [
-  {
-    id: 1,
-    image:
-      "https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 150,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 2,
-    image:
-      "https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg",
-    title: "Kaushalam kalash Copper Pot",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 120,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 3,
-    image:
-      "https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 130,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 4,
-    image:
-      "https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 120,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 1,
-    image:
-      "https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 150,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 2,
-    image:
-      "https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg",
-    title: "Kaushalam kalash Copper Pot",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 120,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 3,
-    image:
-      "https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 130,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-  {
-    id: 4,
-    image:
-      "https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg",
-    title: "Hand Painted Blue Kaushalam Tea Pot in Aluminium",
-    desc: "Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.",
-    price: 120,
-    trendingProductName: "Featured",
-    quantity: 1,
-  },
-];
+import { useContext } from "react";
+import myContext from "../../context/myContext";
+import Loader from "../loader/Loader";
 
 const HomePageProductCard = () => {
   // Navigate to ProductInfo
   const navigate = useNavigate();
+
+  const context = useContext(myContext);
+  const { loading, getAllProduct } = context;
 
   return (
     <>
       <div className="heading">
         <h2>Best Selling Products</h2>
       </div>
+      {loading && <Loader />}
       <div className="product-card-container">
         {/* Product */}
-        {productData.map((item, index) => {
-          const { image, title, price } = item;
+        {getAllProduct.slice(0, 8).map((item, index) => {
+          const { id, title, price, productImageUrl } = item;
           return (
-            <div className="product-card">
+            <div key={index} className="product-card">
               <div
                 className="product-image"
                 onClick={() => navigate("/productinfo")}
               >
-                <img src={image} alt={title} className="product-image" />
+                <img
+                  src={productImageUrl}
+                  alt={title}
+                  className="product-image"
+                />
               </div>
               <div className="product-details">
                 <p className="website-name">ezBuy</p>
                 <h2 className="product-title">{title.substring(0, 25)}</h2>
-                <p className="product-price">${price}</p>
+                <p className="product-price">&#8377;{price}</p>
                 <div className="add-to-cart-btn">
                   <button>Add to Cart</button>
                 </div>
