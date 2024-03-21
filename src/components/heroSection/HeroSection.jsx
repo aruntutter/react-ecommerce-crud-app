2551c16428d80215ba9ee40c603129c2adbf0765
Arun Kumar R <aruntutter19@gmail.com>
2024-03-21 16:14:40 +0530

    removed some text and btn

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -7,14 +7,6 @@ const HeroSection = () => {
       <div className="hero-image">
         <img src={heroImage} alt="Hero" />
       </div>
-      <div className="hero-content">
-        <p>
-          Your <span>one-stop</span> destination for all your shopping needs.
-        </p>
-        <div className="primary-btn">
-          <button>Shop Now</button>
-        </div>
-      </div>
     </div>
   );
 };
